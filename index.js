57e4a05cbf44f0591e3258379ac1657be9ebd553
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-10-25 13:43:21 -0600

    Adding sign-in and sign-up

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log('working')
+'use strict'
+
+const express = require("express");
+const helmet = require('helmet')
+const passport = require('passport')
+const boom = require('@hapi/boom')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const axios = require('axios')
+
+const config = require("./config/config");
+
+const app = express();
+
+// Agregamos las variables de timpo en segundos
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
+// body parser
+app.use(express.json());
+app.use(cookieParser())
+// app.use(session({ session: config.sessionSecret }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+// app.use(helmet())
+// Basic Strategy
+require('./utils/auth/strategies/basic')
+
+const postSignIn = async (req, res, next) => {
+  const { rememberMe } = req.body
+  passport.authenticate('basic', (error, data) => {
+    try {
+      if (error || !data) next(boom.unauthorized())
+      req.login(data, { session: false }, async (error) => {
+        if (error) next(error)
+        const { token, ...user } = data
+
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+        })
+
+        res.status(200).json(user)
+      })
+    } catch (error) {
+      next(error)
+    }
+  })(req, res, next)
+}
+
+const postSignUp = async (req, res, next) => {
+  const { body: user } = req
+  try {
+    await axios({
+      url: `${config.apiUrl}/user`,
+      method: 'post',
+      data: user
+    })
+    res.status(201).json({ message: 'User created' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+app.post("/auth/sign-in", postSignIn)
+app.post("/auth/sign-up", postSignUp);
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
