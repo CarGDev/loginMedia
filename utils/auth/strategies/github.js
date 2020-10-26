e200896dea691b4ec6b84b6bb37c3e46ec1f8aca
MosqueraSt3 <esmosquera66@misena.edu.co>
2020-10-25 23:38:49 -0500

    Facebook & GitHub Auth

---
+++ utils/auth/strategies/github.js
@@ -0,0 +1,41 @@
+const passport = require('passport')
+const axios = require('axios')
+const { get } = require('lodash')
+const boom = require('@hapi/boom')
+
+const { Strategy: GitHubStrategy } = require('passport-github')
+
+const config = require('../../../config/config')
+
+passport.use(
+    new GitHubStrategy({
+    clientID: config.githubClientId,
+    clientSecret: config.githubClientSecret,
+    callbackURL: '/auth/github/callback',
+    includeEmail: true
+  }, async (accessToken, refreshToken, profile, cb) => {  
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/user`,
+            method: "post",
+            data: {
+                nickname: profile.displayName,
+                country: 'CO',
+                postal_code: '00000',
+                birthday: '1900/01/01',
+                status: true,
+                avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.',
+                platform: 'xbox',
+                email: get(profile, 'emails.0.value', `${profile.username}@github.com`),
+                phone: '0000000000',
+                rol: 'usuario',
+                level: '1',
+                password: profile.id,
+                apiKeyToken: config.apiKeyToken
+            }
+        })
+
+        if (!data || status !== 201 ) return cb(boom.unauthorized(), false)
+
+        return cb(null, data)
+    })
+)
