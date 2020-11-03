2e74947edafc3c7daf9236986e109351f91fe2af
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-11-03 13:38:04 -0600

    Modifying index

--- index.js
+++ index.js
@@ -121,14 +121,25 @@ const twitterOAuth = async (req, res, next) => {
   res.cookie('email', email)
   res.cookie('user', username)
   res.cookie('id', id)
-  const query = querystring.stringify({
-    "user": username,
-    "id": id,
-    "token": token,
-    "email": email,
-    "valid": true
+  // const query = querystring.stringify({
+  //   "user": username,
+  //   "id": id,
+  //   "token": token,
+  //   "email": email,
+  //   "valid": true
+  // })
+  // res.redirect('/?' + query)
+  res.status(200).send({
+    error: false,
+    status: 200,
+    body: {
+      'user': username,
+      'id': id,
+      'token': token,
+      'email': email,
+      'valid': true
+    }
   })
-  res.redirect('/?' + query)
   //res.redirect(`${config.apiUrlBack}/`)
 }
 
